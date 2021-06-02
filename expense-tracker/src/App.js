18b6394d93761e89660289cf7cd78f3532024af9
aurimasjurgelis <dev.aurimas@gmail.com>
2021-06-02 21:51:36 +0300

    init

---
+++ expense-tracker/src/App.js
@@ -0,0 +1,14 @@
+import ExpenseItem from './components/ExpenseItem';
+import './App.css';
+
+
+function App() {
+  return (
+    <div>
+      <h2>Let's get started</h2>
+      <ExpenseItem></ExpenseItem>
+    </div>
+  );
+}
+
+export default App;
